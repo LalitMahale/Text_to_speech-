acb8f51c5af65cb820e3a2a52e0021eddf9fad32
LalitMahale <105535508+LalitMahale@users.noreply.github.com>
2022-11-03 14:57:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,25 @@ import streamlit as st
 from gtts import gTTS 
 import os 
 
+st.image(image = "text.jpg")
 
-st.title("Text To Speech Converter ")
-text = st.text_area("Enter Text Here...")
-if st.button("Convert"):
-
-    text_obj = gTTS(text=text, lang="en",slow=False)
+def audio_output(text,lan):
+    text_obj = gTTS(text=text, lang=lan,slow=False)
     text_obj.save("temp.mp3")
-    st.audio("temp.mp3")
+    return st.audio("temp.mp3")
+
+list_lang= ["English","Hindi","Marathi","Gujarati","Malayalam","Tamil",
+"Telugu","Kannada","Urdu","Bengali","French","German","Greek","Rassian"]
+
+lang_dic = {"English":"en","Hindi":"hi","Marathi":"mr","Gujarati":"gu",
+"Malayalam":"ml","Tamil":"ta","Telugu":"te","Urdu":"ur","Bengali":"bn",
+"French":"fr","German":"de","Greek":"el","Kannada":"kn","Rassian":"ru"}
 
 
+
+start = st.selectbox("Select Language",list_lang)
+text = st.text_area("Enter Text Here...")
+lang_code = lang_dic.get(start)
+
+if st.button("Convert"):
+        audio_output(text,lang_code)
